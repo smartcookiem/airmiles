4ae5108a81cfc622e930c6c37a90be157e070a6c
Olga Cookie <angel_2008_year@mail.ru>
2023-01-24 20:43:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 27;
+        int oneMile = 20;
+
+        int bonus = ticket / oneMile;
+
+
+        System.out.println("Заработано бонусных миль - " + bonus);
+
+
+    }
+}
