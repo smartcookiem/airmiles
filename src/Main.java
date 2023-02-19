ea0ff4b80a9c9986eee438e98522ecbf03b89187
Olga Cookie <angel_2008_year@mail.ru>
2023-02-19 19:41:34 +0300

    Code modernization

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 27;
-        int oneMile = 20;
-
-        int bonus = ticket / oneMile;
-
-
-        System.out.println("Заработано бонусных миль - " + bonus);
-
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
+
